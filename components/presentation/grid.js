e3864384153222ebf0cef084594f00262cd1e27b
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-04-18 16:35:03 +0530

    tiny alignment fix

--- components/presentation/grid.js
+++ components/presentation/grid.js
@@ -11,7 +11,7 @@ const Grid = styled.div`
   background-image:
     repeating-linear-gradient(0deg, transparent, transparent 31px, rgba(0, 0, 0, ${gridAlpha}) 31px, rgba(0, 0, 0, ${gridAlpha}) 32px),
     repeating-linear-gradient(-90deg, transparent, transparent 31px, rgba(0, 0, 0, ${gridAlpha}) 31px, rgba(0, 0, 0, ${gridAlpha}) 32px);
-  background-position: left -7px;
+  background-position: left -8px;
   border-radius: 5px 0 0 5px;
 `
 
