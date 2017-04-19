abf78a64c252bd99b0c98630834f0b8a830faf4b
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-04-19 18:19:37 +0530

    min dimensions = 1px

--- components/presentation/component.js
+++ components/presentation/component.js
@@ -6,10 +6,15 @@ import componentSpec from '../../utils/componentSpec'
 const getCSS = (properties) => {
   const css = {}
   Object.keys(componentSpec).map(property => {
-    css[property] = properties[property] || componentSpec[property].default
+    if (typeof properties[property] !== 'undefined') css[property] = properties[property]
+    else css[property] = componentSpec[property].default
+
     css[property] += componentSpec[property].unit || ''
+
     if (property === 'background' && !properties.fill) css.background = 'transparent'
     if (property === 'opacity') css.opacity = properties.opacity / 100
+    if (property === 'width' && css.width === '0px') css.width = '1px'
+    if (property === 'height' && css.height === '0px') css.height = '1px'
   })
   return css
 }
--- utils/componentSpec.js
+++ utils/componentSpec.js
@@ -1,7 +1,7 @@
 const spec = {
   name:             {default: 'component', type: 'text'},
-  width:            {default: 96, type: 'number', min: 32, max: 1024, step: 32, unit: 'px'},
-  height:           {default: 64, type: 'number', min: 32, max: 1024, step: 32, unit: 'px'},
+  width:            {default: 96, type: 'number', min: 0, max: 1024, step: 32, unit: 'px'},
+  height:           {default: 64, type: 'number', min: 0, max: 1024, step: 32, unit: 'px'},
   background:       {default: '#f47373', type: 'color'},
   fill:             {default: 1, type: 'number', min: 0, max: 1, step: 1},
   'border-radius':  {default: 0, type: 'number', min: 0, max: 25, step: 1, displayName: 'rounded', unit: 'px'},
