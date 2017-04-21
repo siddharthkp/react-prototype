6bb6d553e1ee16412208e84351d2e88add9625c2
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-04-21 16:05:56 +0530

    make default background green

--- utils/componentSpec.js
+++ utils/componentSpec.js
@@ -2,7 +2,7 @@ const spec = {
   name:             {default: 'component', type: 'text'},
   width:            {default: 96, type: 'number', min: 0, max: 1024, step: 32, unit: 'px'},
   height:           {default: 64, type: 'number', min: 0, max: 1024, step: 32, unit: 'px'},
-  background:       {default: '#f47373', type: 'color'},
+  background:       {default: '#2EE940', type: 'color'},
   fill:             {default: 1, type: 'number', min: 0, max: 1, step: 1},
   'border-radius':  {default: 0, type: 'number', min: 0, max: 25, step: 1, displayName: 'rounded', unit: 'px'},
   'border-color':   {default: '#f47373', type: 'color'},
